e7e44cb929f5219fbbd16bd3f0b8fb04f4663911
KevinGomez47 <109707848+KevinGomez47@users.noreply.github.com>
2022-08-15 19:34:10 -0500

    Orden de ejecución de Unity
    Se utilizan las funciones de ejecución de Unity Con código para comprobar su estado

--- primerProyectoEBAC/Assets/Scripts/holaMundo.cs
+++ primerProyectoEBAC/Assets/Scripts/holaMundo.cs
@@ -4,21 +4,54 @@ using UnityEngine;
 
 public class holaMundo : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    int x;
+    public GameObject mainCam;
+    // Awake es la primer funcion que se ejecuta al dar play al juego
+    private void Awake()
+    {
+        mainCam = this.gameObject;
+        print("Hola mundo desde Awake");
+        //print("Hola mundo");
+        //Debug.Log("Todo parece ir bien");
+        //Debug.Log("Parece que algo esta queriendo salir mal");
+        //print("Creo que no, todo esta bien");
+        //Debug.LogWarning("No, no, espera!, Creo que si esta saliendo algo mal");
+        //Debug.Log("Olvida, no parece ser nada malo");
+        //Debug.LogError("Vaya! al parecer si estaba algo mal :'D");
+        //x = x + 1;
+        //Debug.Log("X es igual a " + x);
+        
+    }
+    //Start se ejecuta justo despues de Awake
     void Start()
     {
-        print("Hola mundo");
-        Debug.Log("Todo parece ir bien");
-        Debug.Log("Parece que algo esta queriendo salir mal");
-        print("Creo que no, todo esta bien");
-        Debug.LogWarning("No, no, espera!, Creo que si esta saliendo algo mal");
-        Debug.Log("Olvida, no parece ser nada malo");
-        Debug.LogError("Vaya! al parecer si estaba algo mal :'D");
+        Debug.Log("Esta es la primer ejecución de Start");
     }
 
-    // Update is called once per frame
+    // Update se ejecuta cada vez que comienzan a actualizar los cuadros de la pantalla
     void Update()
     {
-        
+        Debug.LogWarning("Se esta actualizando un cuadro");
+    }
+    // FixedUpdate se ejecuta cada 50 cuadros
+    private void FixedUpdate()
+    {
+        Debug.LogError("Han pasado 50 cuadros");
+    }
+    // LateUpdate se ejecutada practicamente al finalizar la carga de los cuadros de la pantalla
+    private void LateUpdate()
+    {
+        print("Esta finalizando la carga de un cuadro");
+    }
+    //OnEnable se ejecuta cuando un GameObject es activado.
+    private void OnEnable()
+    {
+        Debug.Log(mainCam.name + " A sido activado");
+    }
+    // OnDisable se ejecuta cuando un GameObject es desactivado.
+    private void OnDisable()
+    {
+        Debug.LogWarning(mainCam.name + " A sido desactivado");
     }
 }
